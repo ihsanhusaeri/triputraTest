140793b893738dfd89c282f8ee6110e688730e56
ihsanhusaeri <ihusaeri@gmail.com>
2019-08-05 10:42:08 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express= require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.json())
+
+app.get('/api/:number', (req, res)=>{
+    const number = req.params.number
+    let numberString = ["Nol", "Se", "Dua ", "Tiga ", "Empat ", "Lima ", "Enam ", "Tujuh ", "Delapan ", "Sembilan "]
+    
+    let numberSplit = number.split("")
+    let numberSend = new Array()
+    
+    let angka=""
+    for(let i=0; i < numberSplit.length; i++){
+            
+        if(numberSplit[i] != 0){
+            for(let j = 1; j < numberString.length; j++){
+                if(parseInt(numberSplit[i]) == j){
+                    angka+= numberString[j]   
+                }
+            }
+            if(numberSplit.length-i == 4){
+                angka+="ribu"
+            }else if(numberSplit.length-i == 3){
+                angka+="ratus"
+            }else if(numberSplit.length-i == 2){
+                angka+="puluh"
+            }
+            numberSend.push(angka)
+            angka=""
+        }
+    }
+    res.send(numberSend)
+    
+})
+app.listen('5000', () => {console.log("Server running!")})
